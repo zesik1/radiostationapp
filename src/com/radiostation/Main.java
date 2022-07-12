1af5965bf898dbcd82f7016b28d9a1ad0a2695c8
zesik1 <kongaronis@gmail.com>
2022-07-12 15:41:48 +0300

    Creating song objects correct way

--- src/com/radiostation/Main.java
+++ src/com/radiostation/Main.java
@@ -1,37 +1,37 @@
 package com.radiostation;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         performSongs();
+        RadioStation radio = new RadioStation();
+        Scanner scannerObject = new Scanner(System.in);
+        String userChoice = scannerObject.nextLine();
+        System.out.println(radio.getSimilar(userChoice));
 
     }
 
     static void performSongs() {
         Song[] songs = new Song[15];
-        songs[0] = create("Wonderwall", "Oasis", new double[]{90, 10, 0, 0, 0, 0});
-        songs[1] = create("Don't look back in anger", "Oasis", new double[]{80, 0, 10, 0, 0, 0});
-        songs[2] = create("Money", "Pink Floyd", new double[]{50, 20, 10, 0, 20 , 0});
-        songs[3] = create("Catholic girls", "Frank Zappa", new double[]{30, 10, 30, 30, 0 , 0});
-        songs[4] = create("Stairway to heaven", "Led Zeppelin", new double[]{80, 0, 20, 0, 0 , 0});
-        songs[5] = create("Perfect strangers", "Deep Purple", new double[]{100, 0, 0, 0, 0 , 0});
-        songs[6] = create("Purple rain", "Prince", new double[]{10, 80, 10, 0, 0 , 0});
-        songs[7] = create("Back in black", "AC DC", new double[]{100, 0, 0, 0, 0 , 0});
-        songs[8] = create("The thrill is gone", "BB King", new double[]{0, 0, 100, 0, 0 , 0});
-        songs[9] = create("I'd rather go blind", "Etta James", new double[]{0, 0, 60, 40, 0 , 0});
-        songs[10] = create("With or without you", "U2", new double[]{40, 20, 40, 0, 0 , 0});
-        songs[11] = create("Heroes", "David Bowie", new double[]{40, 10, 30, 10, 10 , 0});
-        songs[12] = create("Free Bird", "Lynyrd Skynyrd", new double[]{90, 10, 0, 0, 0, 0});
-        songs[13] = create("Always", "Bon Jovi", new double[]{50, 30, 20, 0, 0, 0});
-        songs[14] = create("Happy when it rains", "The Jesus and Mary Chain", new double[]{40, 60, 0, 0, 0, 0});
-        System.out.println(Arrays.toString(songs[1].getWeights()));
+        songs[0] = new Song("Wonderwall", "Oasis", new double[]{90, 10, 0, 0, 0, 0});
+        songs[1] = new Song("Don't look back in anger", "Oasis", new double[]{80, 0, 10, 0, 0, 0});
+        songs[2] = new Song("Money", "Pink Floyd", new double[]{50, 20, 10, 0, 20 , 0});
+        songs[3] = new Song("Catholic girls", "Frank Zappa", new double[]{30, 10, 30, 30, 0 , 0});
+        songs[4] = new Song("Stairway to heaven", "Led Zeppelin", new double[]{80, 0, 20, 0, 0 , 0});
+        songs[5] = new Song("Perfect strangers", "Deep Purple", new double[]{100, 0, 0, 0, 0 , 0});
+        songs[6] = new Song("Purple rain", "Prince", new double[]{10, 80, 10, 0, 0 , 0});
+        songs[7] = new Song("Back in black", "AC DC", new double[]{100, 0, 0, 0, 0 , 0});
+        songs[8] = new Song("The thrill is gone", "BB King", new double[]{0, 0, 100, 0, 0 , 0});
+        songs[9] = new Song("I'd rather go blind", "Etta James", new double[]{0, 0, 60, 40, 0 , 0});
+        songs[10] = new Song("With or without you", "U2", new double[]{40, 20, 40, 0, 0 , 0});
+        songs[11] = new Song("Heroes", "David Bowie", new double[]{40, 10, 30, 10, 10 , 0});
+        songs[12] = new Song("Free Bird", "Lynyrd Skynyrd", new double[]{90, 10, 0, 0, 0, 0});
+        songs[13] = new Song("Always", "Bon Jovi", new double[]{50, 30, 20, 0, 0, 0});
+        songs[14] = new Song("Happy when it rains", "The Jesus and Mary Chain", new double[]{40, 60, 0, 0, 0, 0});
+//        System.out.println(Arrays.toString(songs[1].getWeights()));
+
 
     }
-    private static Song create(String title, String artist, double[] weights) {
-        Song songs = new Song();
-        songs.setTitle(title);
-        songs.setArtist(artist);
-        songs.setWeights(weights);
-        return songs;
-    }
+
 }
--- src/com/radiostation/RadioStation.java
+++ src/com/radiostation/RadioStation.java
@@ -16,18 +16,15 @@ public class RadioStation {
         this.songs = songs;
     }
     public Song[] getSimilar(String songTitle) {
-// η μέθοδος αυτήν θα πρέπει να επιστρέφει έναν πίνακα με
-// τα τραγούδια του σταθμού διατεταγμένα με βάση την απόστασή
-// τους με το τραγούδι με τον τίτλο που δίνεται. Πρώτα θα
-// είναι το τραγούδι που δόθηκε,
-// δεύτερο αυτό με την μικρότερη απόσταση,
-// τρίτο αυτό με την αμέσως μικρότερη απόσταση κ.ο.κ.
-// Αν δεν μπορεί να βρεθεί το τραγούδι με αυτόν τον τίτλο
-// τότε θα πρέπει να βγαίνει σχετικό μήνυμα λάθους και να
-// επιστρέφεται null.
-// Η απόσταση υπολογίζεται καλώντας την προστατευμένη βοηθητική
-// μέθοδο distance
-        return null;
+        for (Song element : songs) {
+//        for (int i=0; i < 15; i++){
+            if (songTitle.equalsIgnoreCase(element.getTitle())) {
+                System.out.println("song exists");
+            } else {
+                System.out.println("song does not exist");
+            }
+        }
+        return songs;
     }
     protected double distance(Song s1, Song s2) {
 // Επιστρέφει την απόσταση δύο τραγουδιών
