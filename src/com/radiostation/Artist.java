9e1af2638d4ac60803220014a2fcf415e2d48d9f
zesik1 <kongaronis@gmail.com>
2022-07-21 11:46:41 +0300

    Adding the artist class

---
+++ src/com/radiostation/Artist.java
@@ -0,0 +1,13 @@
+package com.radiostation;
+
+public class Artist {
+    private String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
